b4827feb38a29236c267d6c0978c601c2286c2fa
Flakari <natedimock@yahoo.com>
2020-01-19 10:37:34 -0600

    Updates

---
+++ precache-manifest.6c0a40777c3f1b302cd047e71c9c6ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a05f30e30c62fa525267418d26b45d",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "e0d06ed2ff15f86caf91",
+    "url": "/game-of-life/static/css/main.712459f7.chunk.css"
+  },
+  {
+    "revision": "67bd065825f574eeae93",
+    "url": "/game-of-life/static/js/2.596e9f9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-life/static/js/2.596e9f9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0d06ed2ff15f86caf91",
+    "url": "/game-of-life/static/js/main.1045f6a8.chunk.js"
+  },
+  {
+    "revision": "4e0f8a89976a8753cce1",
+    "url": "/game-of-life/static/js/runtime-main.ebd98e23.js"
+  }
+]);
